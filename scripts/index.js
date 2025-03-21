66104b1e64085e7a56f9664bc29006c66a6a25d5
Jecolia <jecoliatieleo@gmail.com>
2025-03-21 09:23:10 +0000

    cloned to wish a birthday

--- scripts/index.js
+++ scripts/index.js
@@ -4,8 +4,8 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Jan 29, 2020',
-  name: 'Darlene'
+  birthdate: 'March 21, 2025',
+  name: 'Schékina La gloire de Dieu'
 };
 
 function hideEverything() {
